317269d2c7340cda6ae653fa77cba9b6dbdc0baa
Ignacio Hernández Montilla <ignaciohrdz@gmail.com>
2021-10-30 13:34:00 +0200

    I tried fitting a Logit model for card number estimation but it didn't work

--- train_card_regression.py
+++ train_card_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-
+import sklearn.metrics as skmetrics
 from utils import get_point_edges
 
 num_cards = 52
@@ -12,8 +12,8 @@ points_coordinates = [k for k in list(card_data.columns.values) if ('_x' in k) o
 
 # Computing the edges (i.e. Euclidean distance between keypoints)
 card_data, edge_cols = get_point_edges(card_data)
-
-feature_cols = points_coordinates + points_visible + edge_cols
+# feature_cols = points_coordinates + points_visible + edge_cols
+feature_cols = points_visible + edge_cols
 
 # Validation set
 val_pct = 0.25
@@ -27,17 +27,22 @@ for i in range(num_cards):
             val_data = pd.concat([val_data, sample_df])
         else:
             val_data = sample_df
+val_images = list(val_data['imageName'].unique())
 lowest_count = val_data['cards_counted'].min()
 X_val = val_data[feature_cols]
 y_val = val_data['cards_counted'] - lowest_count
 
 # Training set
-train_data = card_data[card_data['imageName'].isin(list(val_data['imageName'].unique())) == False]
+train_data = card_data[card_data['imageName'].isin(val_images) == False]
 X_train = train_data[feature_cols]
 y_train = train_data['cards_counted'] - lowest_count
 
 # Fitting a MaxEnt model to predict the probability of the number of cards
 # This was very useful https://cla2019.github.io/scikit_classification.pdf
 clf = LogisticRegression(random_state=0, max_iter=300).fit(X_train, y_train)
-y_pred = clf.predict(X_val)
+y_prob = clf.predict_proba(X_val)
+y_cards = np.matmul(y_prob, np.array([range(lowest_count, num_cards+1)]).T).astype(int).squeeze()
+
+acc = skmetrics.accuracy_score(y_val+lowest_count, y_cards)
+mae = skmetrics.mean_absolute_error(y_val+lowest_count, y_cards)
 
